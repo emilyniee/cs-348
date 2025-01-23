3ff1228081b80f4f6a1e45624a70654a45c49c6d
Jillian Xu <jillian711165@gmail.com>
2025-01-22 20:45:53 -0500

    Update comments and function names

--- milestone-0/backend/database.py
+++ milestone-0/backend/database.py
@@ -1,27 +1,25 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-def get_connection():
-    """Returns a psycopg2 connection"""
-    conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+def get_conn():
+    """Get psycopg2 connection"""
+    conn = psycopg2.connect(DATABASE_URL)
     return conn
 
-def init_db():
+def init():
     """Create a table and insert data"""
-    with get_connection() as conn:
+    with get_conn() as conn:
         with conn.cursor() as cursor:
             with open('./sql_queries/users.sql', 'r') as f:
                 user_query = f.read()
             cursor.execute(user_query)
-
         conn.commit()
 
 def get_all_users():
-    """Query all users from table."""
-    with get_connection() as conn:
+    """Query users from table"""
+    with get_conn() as conn:
         with conn.cursor() as cursor:
             cursor.execute("SELECT * FROM users;")
             results = cursor.fetchall()
--- milestone-0/backend/main.py
+++ milestone-0/backend/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from database import init_db, get_all_users
+from database import init, get_all_users
 
 app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
-    init_db()
+    init()
 
 @app.get("/")
 def read_root():
